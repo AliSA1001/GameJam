78cd7e5512ffc185b1ead85dcb7ac10b94a40940
Ali Al shaiban <ali13579gh@gmail.com>
2026-06-02 19:33:37 +0300

    save

--- Assets/Ali/Scripts/Movement.cs
+++ Assets/Ali/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
     [Header("Move Settings")]
     [SerializeField] private float speed;
     [SerializeField] private float gravity = -9.7f;
+    [SerializeField] private float jumpForce;
 
     private Rigidbody rb;
     private float moveX;
@@ -24,12 +25,22 @@ public class Movement : MonoBehaviour
         rb.linearVelocity = move;
     }
 
-
+    private void Jump()
+    {
+        
+    }
 
     public void MoveInput(InputAction.CallbackContext context)
     {
         moveX = context.ReadValue<Vector2>().x;
         moveY = context.ReadValue<Vector2>().y;
 
+    }
+    public void JumpInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+
+        }
     }
 }
